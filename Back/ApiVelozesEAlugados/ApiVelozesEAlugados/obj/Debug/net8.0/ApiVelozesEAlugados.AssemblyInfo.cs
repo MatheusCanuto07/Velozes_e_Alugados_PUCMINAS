22532cb48b659d5b2059bef06f90286a10e0e969
Matheus Canuto <matheushenriquecanuto77@gmail.com>
2024-06-15 23:50:27 -0300

    Tabela de delete funcionando

--- Back/ApiVelozesEAlugados/ApiVelozesEAlugados/obj/Debug/net8.0/ApiVelozesEAlugados.AssemblyInfo.cs
+++ Back/ApiVelozesEAlugados/ApiVelozesEAlugados/obj/Debug/net8.0/ApiVelozesEAlugados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiVelozesEAlugados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84baa9128e28cf58dda66b6e39224fb3023ceae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09e91ec6068bae8bdb52dfd20dc614d0dc5d000")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiVelozesEAlugados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiVelozesEAlugados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Views/src/pages/menu_inicial_adm/editar/Veiculos/editar_veiculos.js
+++ Views/src/pages/menu_inicial_adm/editar/Veiculos/editar_veiculos.js
@@ -1,6 +1,12 @@
-console.log("pimba")
-
 document.addEventListener('DOMContentLoaded', function () {
+  const inputs = document.querySelectorAll('input, textarea, select');
+  inputs.forEach(input => {
+    input.disabled = true;
+  });
+  carregaDados();
+})
+
+function carregaDados(){
   var url = "https://localhost:7090/api/Carro";
 
   fetch(url)
@@ -8,29 +14,32 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(data => ({ status: response.status, body: data })))
     .then(result => {
       var str = "";
-
       if (result.status === 200) {
         console.log(result.body);
         var carros = JSON.parse(result.body);
         carros.forEach(carro => {
           console.log(carro);
           str += `
-            <tr>
-              <th scope="row">${carro.placa}</th>
-              <td>${carro.modelo}</td>
-              <td>${carro.marca}</td>
-              <td>${carro.cor}</td>
-              <td>${carro.ano}</td>
-              <td>${carro.km}</td>
-              <td>${carro.disponibilidade}</td>
-              <td>${carro.precoKm}</td>
-              <td>${carro.precoDiaria}</td>
-              <td>${carro.observacoes}</td>
-            </tr>
+          <tr>
+            <th scope="row">
+              <input type="text" value="${carro.placa}">
+            </th>
+            <td><input type="text" value="${carro.modelo}"></td>
+            <td><input type="text" value="${carro.marca}"></td>
+            <td><input type="text" value="${carro.cor}"></td>
+            <td><input type="text" value="${carro.ano}"></td>
+            <td><input type="text" value="${carro.km}"></td>
+            <td><input type="text" value="${carro.disponibilidade}"></td>
+            <td><input type="text" value="${carro.precoKm}"></td>
+            <td><input type="text" value="${carro.precoDiaria}"></td>
+            <td><input type="text" value="${carro.observacoes}"></td>
+            <td><button class="btn btn-warning" onclick="editaCarro('${carro.placa}')">Editar</button></td>
+            <td><button class="btn btn-danger" onclick="deletaCarro('${carro.placa}')">Apagar</button></td>
+          </tr>
           `;
         });
         var body_table_carros = document.getElementById("body_table_carros");
-        //body_table_carros.innerHTML = str;
+        body_table_carros.innerHTML = str;
 
         // Seleciona todos os elementos input na página
         const inputs = document.querySelectorAll('input');
@@ -47,5 +56,27 @@ document.addEventListener('DOMContentLoaded', function () {
     .catch(error => {
       console.error("Erro ao fazer a requisição:", error);
     });
+}
 
-})
+function deletaCarro(placa) {
+  console.log("Delete teste")
+
+  fetch(`https://localhost:7090/api/Carro/${placa}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erro ao tentar excluir o carro: ${response.status} - ${response.statusText}`);
+      }
+      console.log('Carro excluído com sucesso.');
+      carregaDados();
+    })
+    .catch(error => {
+      console.error('Erro ao tentar excluir o carro:', error);
+      // Trate o erro conforme necessário
+    });
+
+}
